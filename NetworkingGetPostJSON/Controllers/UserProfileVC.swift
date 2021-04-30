89a4799d52862185f67940e7216afb41c805b653
nikita_kogolenok <nikiton17ns@gmail.com>
2021-05-01 02:01:17 +0300

    Log Out and Fetching Data

--- NetworkingGetPostJSON/Controllers/UserProfileVC.swift
+++ NetworkingGetPostJSON/Controllers/UserProfileVC.swift
@@ -22,9 +22,9 @@ class UserProfileVC: UIViewController {
     lazy var logoutButton: UIButton = {
         let button = UIButton()
         button.frame = CGRect(x: 32,
-                                   y: view.frame.height - 172,
-                                   width: view.frame.width - 64,
-                                   height: 50)
+                              y: view.frame.height - 172,
+                              width: view.frame.width - 64,
+                              height: 50)
         button.backgroundColor = UIColor(hexValue: "#3B5999", alpha: 1)
         button.setTitle("Log Out", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
@@ -81,24 +81,28 @@ extension UserProfileVC {
     private func fetchingUserData() {
         
         if Auth.auth().currentUser != nil {
-            
-            guard let uid = Auth.auth().currentUser?.uid else { return }
-            Database.database().reference()
-                .child("users")
-                .child(uid)
-                .observeSingleEvent(of: .value) { (snapshot) in
-                    
-                    guard let userData = snapshot.value as? [String: Any] else { return }
-                    self.currentUser = CurrentUser(uid: uid, data: userData)
-                    
-                    self.activityIndicator.stopAnimating()
-                    self.userNameLabel.isHidden = false
-                    self.userNameLabel.text = self.getProviderData()
-                    
-                } withCancel: { (error) in
-                    print(error)
-                }
-
+            if let userName = Auth.auth().currentUser?.displayName {
+                activityIndicator.stopAnimating()
+                userNameLabel.isHidden = false
+                userNameLabel.text = getProviderData(with: userName)
+            } else {
+                guard let uid = Auth.auth().currentUser?.uid else { return }
+                Database.database().reference()
+                    .child("users")
+                    .child(uid)
+                    .observeSingleEvent(of: .value) { (snapshot) in
+                        
+                        guard let userData = snapshot.value as? [String: Any] else { return }
+                        self.currentUser = CurrentUser(uid: uid, data: userData)
+                        
+                        self.activityIndicator.stopAnimating()
+                        self.userNameLabel.isHidden = false
+                        self.userNameLabel.text = self.getProviderData(with: self.currentUser?.name ?? "Noname")
+                        
+                    } withCancel: { (error) in
+                        print(error)
+                    }
+            }
         }
     }
     
@@ -115,6 +119,10 @@ extension UserProfileVC {
                     GIDSignIn.sharedInstance()?.signOut()
                     print("User did log out of google")
                     openLoginViewController()
+                case "password":
+                    try! Auth.auth().signOut()
+                    print("User did sign out")
+                    openLoginViewController()
                 default:
                     print("User is signed in with \(userInfo.providerID)")
                 }
@@ -122,7 +130,7 @@ extension UserProfileVC {
         }
     }
     
-    private func getProviderData() -> String {
+    private func getProviderData(with user: String) -> String {
         
         var greetings = ""
         
@@ -134,12 +142,14 @@ extension UserProfileVC {
                     provider = "Facebook"
                 case "google.com":
                     provider = "Google"
+                case "password":
+                    provider = "Email"
                 default:
                     break
                 }
             }
             
-            greetings = "\(currentUser?.name ?? "Noname") Logged in with \(provider!)"
+            greetings = "\(user) Logged in with \(provider!)"
         }
         return greetings
     }
